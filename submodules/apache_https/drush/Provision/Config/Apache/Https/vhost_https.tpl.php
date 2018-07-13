a733936cac9bfba7efc53d6a193f4dbf63261379
helmo <helmo@449000.no-reply.drupal.org>
2018-07-13 10:45:57 +0200

    Issue #2952366 by helmo, antoine.guichard, memtkmcc: Exclude /.well-known/acme-challenge from "HTTPS Required"

--- submodules/apache_https/drush/Provision/Config/Apache/Https/vhost_https.tpl.php
+++ submodules/apache_https/drush/Provision/Config/Apache/Https/vhost_https.tpl.php
@@ -60,6 +60,8 @@ if (sizeof($this->aliases)) {
 <?php
 if ($this->redirection) {
   print " # Redirect all aliases to the selected alias.\n";
+  print " # Except for /.well-known/acme-challenge/ to prevent potential problems with Let's Encrypt\n";
+  print " RewriteCond %{REQUEST_URI} '!/.well-known/acme-challenge/'\n";
   print " RewriteCond %{HTTP_HOST} !^{$this->redirection}$ [NC]\n";
   print " RewriteRule ^/*(.*)$ https://{$this->redirection}/$1 [NE,L,R=301]\n";
 }
